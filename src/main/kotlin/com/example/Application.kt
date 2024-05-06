65cb0a77aad3a4ddde7d351b9a2934eacfff9d1c
MaxiRG <maxiramirezgis@gmail.com>
2024-05-05 23:17:40 -0300

    First endpoints working

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -1,13 +1,24 @@
 package com.example
 
+import com.example.article.controller.article
+import com.example.db.DatabaseSingleton
 import com.example.plugins.*
+import io.ktor.serialization.jackson.*
 import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.routing.*
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
 }
 
 fun Application.module() {
-    configureSerialization()
-    configureRouting()
+    DatabaseSingleton.init()
+    install(ContentNegotiation) {
+        jackson()
+    }
+    install(Routing) {
+        article()
+    }
+
 }
