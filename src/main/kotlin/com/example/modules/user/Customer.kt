9d494259a7809ac2c57f2da502f62a87caea2b4b
M4rkos2002 <marcos.lambertini@ing.austral.edu.ar>
2024-05-11 14:37:16 -0300

    checkout

--- src/main/kotlin/com/example/modules/user/Customer.kt
+++ src/main/kotlin/com/example/modules/user/Customer.kt
@@ -1,12 +1,22 @@
 package com.example.modules.user
 
 import com.example.modules.cart.Cart
+import com.example.modules.cart.Carts
 import kotlinx.serialization.Serializable
+import org.jetbrains.exposed.sql.Table
 
 @Serializable
 class Customer(
     val id: Int,
     val name: String,
-    val cart: Cart,
+    val cartId: Int,
 ) {
+}
+
+object Customers : Table() {
+    val id = integer("id").autoIncrement()
+    val name = varchar("name", 255)
+    val cartId = integer("cart_id") references Carts.id
+
+    override val primaryKey = PrimaryKey(id)
 }
