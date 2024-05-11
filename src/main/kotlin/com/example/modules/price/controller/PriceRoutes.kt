99c35a53a298da94a6a30a440113fe87385daf35
M4rkos2002 <marcos.lambertini@ing.austral.edu.ar>
2024-05-11 13:39:11 -0300

    add entity Price whit table Prices

---
+++ src/main/kotlin/com/example/modules/price/controller/PriceRoutes.kt
@@ -0,0 +1,2 @@
+package com.example.modules.price.controller
+
---
+++ src/main/kotlin/com/example/modules/price/model/Price.kt
@@ -0,0 +1,23 @@
+package com.example.modules.price.model
+
+import kotlinx.serialization.Serializable
+import org.jetbrains.exposed.sql.Table
+
+@Serializable
+class Price(
+    val id: Int,
+    val value: Double,
+    val isUnit: Boolean,
+    val qty: Double? = null,
+) {
+}
+
+object Prices: Table(){
+    val id = integer("id").autoIncrement()
+    val value = double("value")
+    val isUnit = bool("is_unit")
+    val qty = double("qty").nullable()
+
+    override val primaryKey = PrimaryKey(id)
+}
+
