da4718cb96bbf16ef5ae6ec22f291d545ee6fbe4
MaxiRG <maxiramirezgis@gmail.com>
2024-05-29 12:44:35 -0300

    Locust working correctly in docker.

--- locustfile.py
+++ locustfile.py
@@ -1,25 +1,24 @@
 from locust import HttpUser, between, task
 import random
 
+base_path = "http://control-tower-control-tower-1:8080"
+
 
 class Client(HttpUser):
     wait_time = between(1, 5)
-    host = "http://0.0.0.0:8080"
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(args, kwargs)
-        self.address_id = None
-        self.customer_id = None
+    address_id = None
+    customer_id = None
 
     def on_start(self):
-        client_id = self.client.post("/customer", json={"name": "John Doe"})
-        address_id = self.client.post("/address", json={"city": "New York", "road": "5th Avenue", "number": 123})
+        client_id = self.client.post(base_path + "/customer", json={"name": "John Doe"})
+        address_id = self.client.post(base_path + "/address",
+                                      json={"city": "New York", "road": "5th Avenue", "number": 123})
         self.customer_id = client_id.json().get("id")
         self.address_id = address_id.json().get("id")
 
     @task
     def create_order(self):
-        self.client.post("/order", json={
+        self.client.post(base_path + "/order", json={
             "productsId": [
                 {
                     "productId": 1,
@@ -31,56 +30,36 @@ class Client(HttpUser):
                 }
             ],
             "addressId": self.address_id,
-            "customerId": self.customer_id,
-            "total": 89.99
+            "customerId": self.customer_id
         })
 
 
 class StoreAdmin(HttpUser):
     wait_time = between(1, 5)
-    host = "http://0.0.0.0:8080"
+    product_ids = []
+    inventory_ids = []
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(args, kwargs)
-        self.product_ids = []
-        self.inventory_ids = []
-
-    @task(1)
+    @task
     def create_product(self):
-        product_id = self.client.post("/product", json={"price": 1.0, "name": "bread"})
-        self.product_ids.append(product_id.json().get("id"))
-
-    @task(1)
-    def create_inventory(self):
-        if len(self.product_ids) == 0:
-            return
-        rnd = random.randint(0, len(self.product_ids) - 1)
-        inventory_id = self.client.post("/inventory", json={"productId": self.product_ids[rnd], "stock": 10})
-        self.inventory_ids.append(inventory_id.json().get("id"))
-
-    @task(5)
-    def add_stock(self):
-        if len(self.product_ids) == 0:
-            return
-        if len(self.inventory_ids) == 0:
-            return
-        rnd = random.randint(0, len(self.product_ids) - 1)
-        rndStock = random.randint(0, 60)
-        self.client.patch("/addStock", json={"productId": self.product_ids[rnd], "stockToAdd": rndStock})
+        product_id = self.client.post(base_path + "/product", json={"price": 1.0, "name": "bread"})
+        p_id = product_id.json().get("id")
+        inventory_id = self.client.post(base_path + "/inventory", json={"productId": p_id, "stock": 10})
+        i_id = inventory_id.json().get("productId")
+        rndStock = random.randint(1, 60)
+        self.client.patch(base_path + "/inventory/addStock", json={"productId": i_id, "stockToAdd": rndStock})
 
 
 class ControlTowerCoordinator(HttpUser):
     wait_time = between(1, 5)
-    host = "http://0.0.0.0:8080"
 
     @task
     def get_all_orders(self):
-        self.client.get("/order")
+        self.client.get(base_path + "/order")
 
     @task
     def get_all_inventories(self):
-        self.client.get("/inventory")
+        self.client.get(base_path + "/inventory/")
 
     @task
     def get_all_deliveries(self):
-        self.client.get("/delivery")
+        self.client.get(base_path + "/delivery")
